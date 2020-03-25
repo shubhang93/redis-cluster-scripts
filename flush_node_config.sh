4a8f92eef58b1cf54500ca5d244465960cae1bda
shubhang <shubhang.sabbath@gmail.com>
2020-03-25 19:21:43 +0530

    fix flush script

--- flush_node_config.sh
+++ flush_node_config.sh
@@ -5,6 +5,6 @@ CLUSTER_PORTS=(7000 7001 7002 7003 7004 7005)
 
 for port in "${CLUSTER_PORTS[@]}";do
   rm "./$port/nodes.conf"
-  printf "Flushed %s conf\n" "$port"
+	eval redis-cli "-p $port flushall"
   done
 
